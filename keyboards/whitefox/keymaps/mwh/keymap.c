e463d40a0f8f5156890412bb9a0ebf0d5f39e06b
Matt Hoffman <mhoffman@gmail.com>
2021-07-25 14:40:36 +0100

    Update keymap to use LAYOUT_vanilla.

--- keyboards/whitefox/keymaps/mwh/keymap.c
+++ keyboards/whitefox/keymaps/mwh/keymap.c
@@ -20,67 +20,64 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CTL_ESC LCTL_T(KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* 
-     * Default layer.
-     * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────────────┬───────┐
-     * │Esc_Grv│1      │2      │3      │4      │5      │6      │7      │8      │9      │0      │-      │=      │        Bckspc │Grv    │
-     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────────────┼───────┤
-     * │Tab    │Q      │W      │E      │R      │T      │Y      │U      │I      │O      │P      │[      │]      │        Bcksls │Del    │
-     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┴───────────────┼───────┤
-     * │Ctl_Esc│A      │S      │D      │F      │G      │H      │J      │K      │L      │;      │'      │        Enter          │PgUp   │
-     * ├───────┴───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────────────┬───────┼───────┤
-     * │Shift          │Z      │X      │C      │V      │B      │N      │M      │,      │.      │/      │        Shift  │Up     │PgDown │
-     * ├───────┬───────┼───────┼───────┴───────┴───────┴───────┴───────┴───────┴───────┼───────┼───────┼───────┬───────┼───────┼───────┤
-     * │Ctrl   │Alt    │Gui    │Space                                                  │Gui    │Alt    │Fn     │Left   │Down   │Right  │
-     * └───────┴───────┴───────┴───────────────────────────────────────────────────────┴───────┴───────┴───────┴───────┴───────┴───────┘
+    /*
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
+     * │ESC│ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │BACKSPC│ ` │
+     * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┼───┤
+     * │TAB  │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │  \  │DEL│
+     * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┼───┤
+     * │CTRL  │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │ ENTER  │PUP│
+     * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┼───┤
+     * │SHIFT   │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │SHIFT │   │PDN│
+     * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬┴──┬───┼───┼───┤
+     * │CTL │ALT │GUI │         SPACE          │GUI│ALT│FN │   │   │   │
+     * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
      */
-    [0] = LAYOUT( \
-        KC_GESC,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, _______,KC_BSPC,KC_GRV, \
-        KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,        KC_BSLS,KC_DEL, \
-        CTL_ESC,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,_______,KC_ENT,         KC_PGUP,\
-        KC_LSFT,_______,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,        KC_RSFT,KC_UP,  KC_PGDN,\
-        KC_LCTL,KC_LALT,KC_LGUI,KC_SPC,                                                 KC_LGUI,KC_LALT,MO(1),  KC_LEFT,KC_DOWN,KC_RGHT \
+    [0] = LAYOUT_vanilla( \
+        KC_GESC,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC,KC_GRV, \
+        KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,KC_DEL, \
+        CTL_ESC,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT,         KC_PGUP,\
+        KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,        KC_UP,  KC_PGDN,\
+        KC_LCTL,KC_LALT,KC_LGUI,KC_SPC,                                 KC_LGUI,KC_LALT,MO(1),          KC_LEFT,KC_DOWN,KC_RGHT \
     ),
     /*
-     * Media layer.
-     * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────────────┬───────┐
-     * │       │F1     │F2     │F3     │F4     │F5     │F6     │F7     │F8     │F9     │F10    │F11    │F12    │        BL_togg│       │
-     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────────────┼───────┤
-     * │       │       │       │       │       │       │       │       │       │       │       │BL_dec │BL_inc │        BL_step│Mute   │
-     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┴───────────────┼───────┤
-     * │       │       │       │       │       │       │       │       │       │       │       │       │                       │Home   │
-     * ├───────┴───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────────────┬───────┼───────┤
-     * │               │       │       │       │       │       │       │       │       │       │       │               │VolUp  │End    │
-     * ├───────┬───────┼───────┼───────┴───────┴───────┴───────┴───────┴───────┴───────┼───────┼───────┼───────┬───────┼───────┼───────┤
-     * │       │       │       │Play                                                   │       │       │       │Rewind │VolDown│FFward │
-     * └───────┴───────┴───────┴───────────────────────────────────────────────────────┴───────┴───────┴───────┴───────┴───────┴───────┘
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
+     * │   │F1 │F2 │F3 │F4 │F5 │F6 │F7 │F8 │F9 │F10│F11│F12│   TOGG│   │
+     * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┼───┤
+     * │     │   │   │   │   │   │   │   │   │   │   │DEC│INC│ STEP│MUT│
+     * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┼───┤
+     * │      │   │   │   │   │   │   │   │   │   │   │   │        │HOM│
+     * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┼───┤
+     * │        │   │   │   │   │   │   │   │   │   │   │      │VUP│END│
+     * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬┴──┬───┼───┼───┤
+     * │    │    │    │         PLAY           │   │   │   │REW│VDN│FFD│
+     * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
      */
-    [1] = LAYOUT( \
-        XXXXXXX,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,BL_TOGG,XXXXXXX,\
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,BL_DEC, BL_INC,         BL_STEP,KC_MUTE,\
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______,XXXXXXX,        KC_HOME,\
-        XXXXXXX,_______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,        XXXXXXX,KC_VOLU,KC_END, \
-        XXXXXXX,XXXXXXX,XXXXXXX,KC_MPLY,                                                XXXXXXX,MO(2),  XXXXXXX,KC_MRWD,KC_VOLD,KC_MFFD \
+    [1] = LAYOUT_vanilla( \
+        XXXXXXX,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, BL_TOGG,XXXXXXX,\
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,BL_DEC, BL_INC, BL_STEP,KC_MUTE,\
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,        KC_HOME,\
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,        XXXXXXX,KC_VOLU,KC_END, \
+        XXXXXXX,XXXXXXX,XXXXXXX,KC_MPLY,                                        XXXXXXX,MO(2),  XXXXXXX,KC_MRWD,KC_VOLD,KC_MFFD \
     ),
     /* 
-     * Special layer.
-     * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────────────┬───────┐
-     * │       │       │       │       │       │       │       │       │       │       │       │       │       │               │       │
-     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────────────┼───────┤
-     * │       │       │       │       │       │       │       │       │       │       │       │       │       │               │       │
-     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┴───────────────┼───────┤
-     * │       │       │       │       │Flash  │       │       │       │       │       │       │       │                       │       │
-     * ├───────┴───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────────────┬───────┼───────┤
-     * │               │       │       │       │       │       │       │       │       │       │       │               │       │       │
-     * ├───────┬───────┼───────┼───────┴───────┴───────┴───────┴───────┴───────┴───────┼───────┼───────┼───────┬───────┼───────┼───────┤
-     * │       │       │       │                                                       │       │       │       │       │       │       │
-     * └───────┴───────┴───────┴───────────────────────────────────────────────────────┴───────┴───────┴───────┴───────┴───────┴───────┘
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
+     * │   │   │   │   │   │   │   │   │   │   │   │   │   │       │   │
+     * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┼───┤
+     * │     │   │   │   │   │   │   │   │   │   │   │   │   │     │   │
+     * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┼───┤
+     * │      │   │   │   │RST│   │   │   │   │   │   │   │        │   │
+     * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┼───┤
+     * │        │   │   │   │   │   │   │   │   │   │   │      │   │   │
+     * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬┴──┬───┼───┼───┤
+     * │    │    │    │                        │   │   │   │   │   │   │
+     * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
      */
-    [2] = LAYOUT( \
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______,XXXXXXX,XXXXXXX,\
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,        XXXXXXX,XXXXXXX,\
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,RESET,  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______,XXXXXXX,        XXXXXXX,\
-        XXXXXXX,_______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,        XXXXXXX,XXXXXXX,XXXXXXX,\
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                                                XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX \
+    [2] = LAYOUT_vanilla( \
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,\
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,\
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,RESET,  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,        XXXXXXX,\
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,        XXXXXXX,XXXXXXX,XXXXXXX,\
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                                        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX \
     ),
 };
