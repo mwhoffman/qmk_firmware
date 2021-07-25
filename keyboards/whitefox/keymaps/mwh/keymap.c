addbad4778ac3649466bcba78f8c2c4ceece6961
Matt Hoffman <mhoffman@gmail.com>
2021-07-25 18:29:25 +0100

    Fix email address typo.

--- keyboards/whitefox/keymaps/mwh/keymap.c
+++ keyboards/whitefox/keymaps/mwh/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 Matt Hoffman <mwhoffman@gmail.com>
+Copyright 2021 Matt Hoffman <mhoffman@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
